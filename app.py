ea83027e2815f87a2782e09f68888534e5acfd54
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-05 19:48:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def pull(update, context):
 	if update.effective_message.chat_id == 208339045:
 		rep = update.effective_message.text.split(' ')[1]
 		PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
-		x = subprocess.run(['git', 'pull'], cwd=PATH_OF_GIT_REPO, text=True)
+		x = subprocess.run(['git', 'pull'], cwd=PATH_OF_GIT_REPO, capture_output=True)
 		
-		context.bot.send_message(update.effective_message.chat_id, x, parse_mode='HTML')
+		context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
 
 
 

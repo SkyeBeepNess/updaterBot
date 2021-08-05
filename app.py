3cdd0900a12b1686a5400388e097eab5a4af30f3
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-05 20:55:12 +0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,18 @@ def pull(update, context):
 		x = subprocess.run(['git', 'pull'], cwd=PATH_OF_GIT_REPO, capture_output=True)
 		
 		context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
+	else:
+		context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
 
+def restart(update, context):
+	if update.effective_message.chat_id == 208339045:
+		rep = update.effective_message.text.split(' ')[1]
+		PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
+		x = subprocess.run(['pkill', '-9', '-f'], cwd=PATH_OF_GIT_REPO, capture_output=True)
+		
+		context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
+	else:
+		context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
 
 
 
@@ -26,7 +37,8 @@ def main():
 	updater = Updater("1410913727:AAHS0MygZhyD_I_FI3OHUYLW-pWg3y9OTR0", use_context=True)
 	dispatcher = updater.dispatcher
 
-	dispatcher.add_handler(CommandHandler('pull', pull))    				  
+	dispatcher.add_handler(CommandHandler('pull', pull)) 
+	dispatcher.add_handler(CommandHandler('restart', restart))    				  
 
 
 

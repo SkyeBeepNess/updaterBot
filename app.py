b790769f78fdfb09eda7a95e4da78bcfcd7dcd5c
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-06 14:18:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def main():
 					context.bot.send_message(update.effective_message.chat_id, "turning the bot off...", parse_mode='HTML')
 					time.sleep(10)
 					restarted = subprocess.run(['python3', 'app.py'], cwd=PATH_OF_GIT_REPO, capture_output=True)
-					context.bot.send_message(update.effective_message.chat_id, restarted.stdout.decode(), parse_mode='HTML')
+					#context.bot.send_message(update.effective_message.chat_id, restarted.stdout.decode(), parse_mode='HTML')
 					context.bot.send_message(update.effective_message.chat_id, "the bot is up and runing! (it seems so anyway)", parse_mode='HTML')
 					break
 				else: 

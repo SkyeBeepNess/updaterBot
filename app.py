44aa9a482f4bcf488574c2f94728474a1546f37d
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-06 12:33:42 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,36 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHa
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
-def pull(update, context):
-	if update.effective_message.chat_id == 208339045:
-		rep = update.effective_message.text.split(' ')[1]
-		PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
-		x = subprocess.run(['git', 'pull'], cwd=PATH_OF_GIT_REPO, capture_output=True)
-		
-		context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
-		context.bot.send_message(update.effective_message.chat_id, "hello world", parse_mode='HTML')
-	else:
-		context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
-
-def restart(update, context):
-	if update.effective_message.chat_id == 208339045:
-		rep = update.effective_message.text.split(' ')[1]
-		PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
-		x = subprocess.run(['pkill', '-9', '-f', 'app.py'], cwd=PATH_OF_GIT_REPO, capture_output=True)
-		
-		context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
-	else:
-		print('wf')
-		#context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
-
-
-
-
-
-
 def main():
-
-
 	updater = Updater("1410913727:AAHS0MygZhyD_I_FI3OHUYLW-pWg3y9OTR0", use_context=True)
 	dispatcher = updater.dispatcher
+
+
+	def pull(update, context):
+		if update.effective_message.chat_id == 208339045:
+			rep = update.effective_message.text.split(' ')[1]
+			PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
+			x = subprocess.run(['git', 'pull'], cwd=PATH_OF_GIT_REPO, capture_output=True)
+			
+			context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
+			context.bot.send_message(update.effective_message.chat_id, "hello world", parse_mode='HTML')
+		else:
+			context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
+
+	def restart(update, context):
+		if update.effective_message.chat_id == 208339045:
+			rep = update.effective_message.text.split(' ')[1]
+			PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
+			#x = subprocess.run(['pkill', '-9', '-f', 'app.py'], cwd=PATH_OF_GIT_REPO, capture_output=True)
+			updater.stop()
+	        os.execl(sys.executable, sys.executable, *sys.argv)
+			context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
+		else:
+			print('wf')
+			#context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
+
+
+
 
 	dispatcher.add_handler(CommandHandler('pull', pull)) 
 	dispatcher.add_handler(CommandHandler('restart', restart))    				  

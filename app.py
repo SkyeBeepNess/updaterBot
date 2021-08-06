ffa240c4e44801519473844ee885e9bb6997e196
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-06 12:38:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ def main():
 			rep = update.effective_message.text.split(' ')[1]
 			PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
 			#x = subprocess.run(['pkill', '-9', '-f', 'app.py'], cwd=PATH_OF_GIT_REPO, capture_output=True)
-			update.message.reply_text('Bot is restarting...')
-        	Thread(target=stop_and_restart).start()
+				update.message.reply_text('Bot is restarting...')
+				Thread(target=stop_and_restart).start()
 			context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
 		else:
 			print('wf')

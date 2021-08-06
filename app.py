153eed2bc72a671f876166daa2f0369aa5fc6c5a
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-06 13:35:49 +0500

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def main():
 
 	def restart(update, context):
 		if update.effective_message.chat_id == 208339045:
-			#rep = update.effective_message.text.split(' ')[1]
+			rep = update.effective_message.text.split(' ')[1]
 			#PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
-			#x = subprocess.run(['pkill', '-9', '-f', 'app.py'], cwd=PATH_OF_GIT_REPO, capture_output=True)
+			x = subprocess.run(['pidof', 'python3'], capture_output=True)
 
-			context.bot.send_message(update.effective_message.chat_id, 'eheh', parse_mode='HTML')
+			context.bot.send_message(update.effective_message.chat_id, x.stdout.decode(), parse_mode='HTML')
 		else:
 			print('wf')
 			#context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')

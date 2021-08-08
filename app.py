22f7adaf28d0a7b1d3259f1688d34eacaec1977f
SkyeBeepNess <60771619+SkyeBeepNess@users.noreply.github.com>
2021-08-08 14:08:33 +0500

    test

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-import subprocess
-import telegram
-import logging
 import time
+import config
+import logging
+import telegram
+import subprocess
 from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, Filters, PicklePersistence
 #skye_BeepNess
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -64,28 +65,32 @@ def main():
 
 	def kill(update, context):
 		if update.effective_message.chat_id == 208339045:
+			lz=[]
 			rep = update.effective_message.text.split(' ')[1]
 			PATH_OF_GIT_REPO = f'/home/beepuser/Documents/bots/{rep}'
 			pidof = subprocess.run(['pidof', 'python3'], capture_output=True)
 			for i in pidof.stdout.decode().split(' '):
-
 				pwdx = subprocess.run(['pwdx', i], capture_output=True)
 				l = pwdx.stdout.decode().split("/")
 				l.reverse()
-				lz = l[0].strip()
+				lz.append(l[0].strip())
 			
-				if lz == rep:
-					killed = subprocess.run(['kill', '-2', i], capture_output=True)
-					context.bot.send_message(update.effective_message.chat_id, "turning the bot off...", parse_mode='HTML')
-					time.sleep(5)
-					context.bot.send_message(update.effective_message.chat_id, "the bot is off!", parse_mode='HTML')
-					break
-				else: 
-					context.bot.send_message(update.effective_message.chat_id, "this bot isn't running anyway", parse_mode='HTML')
-
+			if rep in lz:
+				killed = subprocess.run(['kill', '-2', i], capture_output=True)
+				context.bot.send_message(update.effective_message.chat_id, "turning the bot off...", parse_mode='HTML')
+				while rep in lz:
+					lz=[]
+					for i in pidof.stdout.decode().split(' '):
+						pwdx = subprocess.run(['pwdx', i], capture_output=True)
+						l = pwdx.stdout.decode().split("/")
+						l.reverse()
+						lz.append(l[0].strip())
+				context.bot.send_message(update.effective_message.chat_id, "the bot is now off!", parse_mode='HTML')
+			else: 
+				context.bot.send_message(update.effective_message.chat_id, "this bot isn't running anyway", parse_mode='HTML')
 
 		else:
-			print('wf')
+			print("It seems like you aren't allowed to do that")
 			#context.bot.send_message(update.effective_message.chat_id, "YOU SHALL NOT PASS", parse_mode='HTML')
 
 
